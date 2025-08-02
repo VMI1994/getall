f6325835199f10a3151acb9b24542a75e938fceb
Just Playin <ensminger@yahoo.com>
2025-08-02 18:57:51 -0400

    Add files via upload

---
+++ getall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git clone https://github.com/VMI1994/linux-setup.git
+cd
+git clone https://github.com/VMI1994/Docker-Portainer.git
+cd
+git clone https://github.com/VMI1994/Nodejs.git
+cd
+git clone https://github.com/VMI1994/magic-mirror-setup.git
+cd
+git clone https://github.com/VMI1994/xrdpubuntu.git
+clear
+ls -lha
+exit
+
